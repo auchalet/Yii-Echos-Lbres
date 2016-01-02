c50c96381e662819f00b3b1e3bfd530ff7020606
Devow <alexis.michallet@gmail.com>
2016-01-02 19:41:09 +0100

    Ajout page site en construction

---
+++ frontend/views/site/build.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Site en construction</title>
+	
+    <!-- css -->
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link href="assets/8e8udv3/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/8e8udv3/css/animations.css" rel="stylesheet" />
+    <link href="assets/8e8udv3/css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+	
+	<section class="hero" id="intro">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12 text-right navicon">
+                  <a id="nav-toggle" class="nav_slide_button" href="#"><span></span></a>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center inner">
+					<div class="animatedParent">
+						<h1 class="animated fadeInDown">ECHOS LIBRES</h1>
+						<p class="animated fadeInUp">Notre site sera accessible prochainement !</br>"Tout vient à point à qui sait attendre"</p></br>
+					</div>
+			   </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 col-md-offset-3 text-center">
+                  <div class="input-group input-group-lg">
+                    <span class="input-group-addon" id="sizing-addon1">Demande d'inscription</span>
+                    <input type="text" class="form-control" placeholder="@votremail" aria-describedby="sizing-addon1">
+                  </div>
+                </div>
+              </div>
+            </div>
+    </section>
+	
+
+    <!-- Core JavaScript Files -->
+    <script src="assets/8e8udv3/js/jquery.min.js"></script>	 
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="assets/8e8udv3/js/jquery.sticky.js"></script>
+	<script src="assets/8e8udv3/js/css3-animate-it.js"></script>
+
+</body>
+
+</html>
