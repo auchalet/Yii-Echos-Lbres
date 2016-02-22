2f5cc2b603c5fa63d1bf0cad162757bc753517f2
auchalet <alexis.michallet@gmail.com>
2016-02-22 22:11:19 +0100

    Préparation page création

--- backend/views/page/_formPage.php
+++ backend/views/page/_formPage.php
@@ -7,20 +7,35 @@ use yii\widgets\ActiveForm;
 /* @var $model common\models\SitePage */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-
+<pre>Faire des blocs qu'on peut cacher en JS // En bas, bloc SEO affiché pour le membre SEO (metas/slug)</pre>
 <div class="site-page-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div>
+        <p>Titre de la page</p>
+        <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+    </div>
+    
+    <div>
+        <p>Titres</p>
+        
+        <?= $form->field($model, 'h1')->textarea(['rows' => 6])->label('Titre principal') ?>
 
-    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'h2')->textarea(['rows' => 6])->label('Sous-titre') ?>
+    </div>
 
-    <?= $form->field($model, 'h1')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'h2')->textarea(['rows' => 6]) ?>
+    <div>
+        <p>Contenu</p>
+        <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+    </div>
 
-    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+    <div>
+        <p>Options SEO</p>
+        <?= $form->field($model, 'slug')->textInput(['maxlength' => true])->label('URL') ?>
+        <?= $form->field($model, 'meta_description')->textarea(['rows' => 6]) ?>
+        <?= $form->field($model, 'meta_keywords')->textarea(['rows' => 6]) ?>
+    </div>
 
     <?= $form->field($model, 'status')->textInput() ?>
 
@@ -28,11 +43,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'updated_at')->textInput() ?>
 
-    <?= $form->field($model, 'meta_description')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'meta_keywords')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'category_id')->textInput() ?>
+    <?= $form->field($model, 'category_id')->textInput(['value' => '6']) ?>
 
     <?= $form->field($model, 'user_id')->textInput() ?>
 
